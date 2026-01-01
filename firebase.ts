e20cc82d2d154cd23b561bdf3fa8b25d153efaa4
Hendwriting-Converter62-hub <bandarban62@gmail.com>
2026-01-01 16:54:06 +0600

    Fix typo in firebaseConfig declaration

--- firebase.ts
+++ firebase.ts
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
+cconst firebaseConfig = {
   apiKey: "AIzaSyCANuI_7iQGvvBH5YmVIa3p_yUPw8oG_B0",
   authDomain: "firstsight-8f660.firebaseapp.com",
   databaseURL: "https://firstsight-8f660-default-rtdb.firebaseio.com",
@@ -13,7 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "517418928090",
   appId: "1:517418928090:web:f93583c4ab9b9be7c822e3"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
